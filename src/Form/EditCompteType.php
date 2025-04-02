fb875adf0f5df44d6b5b557420debf6252336e2b
EyaMahjoub <mahjouba562@gmail.com>
2025-04-02 22:44:37 +0100

    form

---
+++ src/Form/EditCompteType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Passager;
+use App\Entity\Vole;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EditCompteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('email')
+            ->add('telephone')
+            ->add('reservation')
+            ->add('vole', EntityType::class, [
+                'class' => Vole::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Passager::class,
+        ]);
+    }
+}
